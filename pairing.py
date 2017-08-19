c51471aa6965ae22ba8561376eea9872efe59627
ccquiambao <ccquiambao@gmail.com>
2017-08-19 16:58:09 -0700

    minor cleaning pairing.py

--- pairing.py
+++ pairing.py
@@ -4,27 +4,7 @@ import random
 import pickle
 import sys
 
-def make_pairs(student_lst):
-    random.shuffle(student_lst)
-    for ind, student in enumerate(student_lst[:-3], 1):
-        print '{:12}'.format(student.strip()),
-        if not ind % 2:
-            print
-
-    for student in student_lst[-3:]:
-        print '{:12}'.format(student),
-
-def make_pairs2(student_lst):
-    random.shuffle(student_lst)
-    for student1, student2 in zip(student_lst[::2], student_lst[1::2]):
-        student1 =  student1.strip()
-        student2 =  student2.strip()
-
-        print '{:10} {:10}'.format(student1, student2)
-    if len(student_lst) % 2:
-        print '{:10}'.format(student_lst[-3], student_lst[-2], student_lst[-1])
-
-def make_pairs3(student_lst, pair_dict):
+def make_pairs(student_lst, pair_dict):
     low_score = sum(pair_dict.itervalues()) + 1000
     final_lst = None
     final_dict = None
@@ -44,7 +24,6 @@ def make_pairs3(student_lst, pair_dict):
         for pair in pairs:
             temp_dict[pair] += 1
 
-
         cur_score = sum(temp_dict.itervalues())
         if cur_score < low_score:
             final_lst = student_lst[:]
@@ -56,6 +35,7 @@ def make_pairs3(student_lst, pair_dict):
     else:
         return student_lst, temp_dict
 
+
 def print_pairs(student_lst):
     for ind, student in enumerate(student_lst[:-3], 1):
         print '{:12}'.format(student.strip()),
@@ -64,9 +44,6 @@ def print_pairs(student_lst):
 
     for student in student_lst[-3:]:
         print '{:12}'.format(student),
-
-def print_list(student_lst):
-    return [student for student in student_lst]
 
 
 if __name__ == '__main__':
@@ -77,7 +54,7 @@ if __name__ == '__main__':
     with open('pair_dict.pkl', 'r') as f:
         pair_dict = pickle.load(f)
 
-    pairs, updated_dict = make_pairs3(students, pair_dict)
+    pairs, updated_dict = make_pairs(students, pair_dict)
     print_pairs(pairs)
     print sum(updated_dict.itervalues())
 
