72e69312f5572337c884320b0deaad37cc572f12
ccquiambao <ccquiambao@gmail.com>
2017-08-24 13:44:37 -0700

    updated pairing.py to force student into triplet

--- pairing.py
+++ pairing.py
@@ -4,21 +4,23 @@ import random
 import pickle
 import sys
 
-def make_pairs(student_lst, pair_dict):
-    low_score = sum(pair_dict.itervalues()) + 1000
+def make_pairs(student_lst, pair_dict, force_triple=False):
+    scores = pair_dict.itervalues()
+    low_score = float('inf')
     final_lst = None
     final_dict = None
-    test_sum = 0
-    for _ in xrange(100000):
-        test =  pair_dict[('Brayden', 'Jenny')]
-        if test > 1:
-            print test
+
+    for _ in xrange(10000):
+
         random.shuffle(student_lst)
         pair_gen = itertools.izip(student_lst[:-3:2], student_lst[1:-3:2])
         pairs = [tuple(sorted([student1, student2])) for (student1, student2) in pair_gen]
 
-
         remains = itertools.combinations(student_lst[-3:], 2)
+        if force_triple:
+            if (force_triple not in student_lst[-3]):
+                continue
+
         for student1, student2 in remains:
             pair = sorted([student1, student2])
             pair = tuple(pair)
@@ -28,21 +30,20 @@ def make_pairs(student_lst, pair_dict):
         for pair in pairs:
             temp_dict[pair] += 1
 
+        scores = temp_dict.itervalues()
+        cur_score = sum([score ** 2 for score in scores])
 
-        test = temp_dict[('Brayden', 'Jenny')]
-
-        if test > 1:
-            test_sum += 1
-
-        cur_score = sum(temp_dict.itervalues())
-        if cur_score != 53:
-            print cur_score
         if cur_score < low_score:
             final_lst = student_lst[:]
             final_dict = temp_dict.copy()
+            low_score = cur_score
 
-    print test_sum
     if final_lst and final_dict:
+        if final_dict[('Brayden', 'Jenny')] != 2:
+            print final_dict[('Brayden', 'Jenny')]
+
+        if sum(final_dict.itervalues()) != 53:
+            print sum(final_dict.itervalues())
         return final_lst, final_dict
 
     else:
@@ -67,12 +68,18 @@ if __name__ == '__main__':
     with open('pair_dict.pkl', 'r') as f:
         pair_dict = pickle.load(f)
 
-    pairs, updated_dict = make_pairs(students, pair_dict)
+    if len(sys.argv) == 2:
+        triplet = sys.argv[1]
+    else:
+        triplet = False
+
+    pairs, updated_dict = make_pairs(students, pair_dict, force_triple=triplet)
     print_pairs(pairs)
+    print
     print sum(updated_dict.itervalues())
 
-    confirm = raw_input('List okay?')
-    if confirm != 'y':
-        sys.exit()
+    # confirm = raw_input('List okay?')
+    # if confirm != 'y':
+    #     sys.exit()
     # with open('pair_dict.pkl', 'w') as f:
     #     pickle.dump(updated_dict, f)
